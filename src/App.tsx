0a7705236aeda6a76f0be5343abb7af7cd85061f
Kaumudi Rawal <rawal.k@northeastern.edu>
2024-07-02 22:12:23 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Labs from './Labs';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To Web Dev</h1>
+      <h2>By Kaumudi Rawal</h2>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
