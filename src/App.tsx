ce174ab84a2216d4538c5e47ffc786aca53b4aa3
KaumudiRRawal <kaumudirrawal@gmail.com>
2024-07-05 18:41:44 -0400

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import Labs from './Labs';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome To Web Dev</h1>
-      <h2>By Kaumudi Rawal</h2>
+    <div>
       <Labs />
     </div>
   );
